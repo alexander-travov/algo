4ffe94af6fdc7ddbf37e6a65b2b059dfc4b48dc8
Alexander Travov <alexander.travov@gmail.com>
2019-06-07 00:19:17 +0300

    Minimum Unsorted Subarray problem.

---
+++ InterviewBit/Arrays/MinimumUnsortedSubarray.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Minimum Unsorted Subarray
+=========================
+You are given an array (zero indexed) of N non-negative integers, A0, A1 ,…, AN-1.
+Find the minimum sub array Al, Al+1 ,…, Ar so if we sort(in ascending order) that sub array, then the whole array should get sorted.
+If A is already sorted, output -1.
+"""
+
+from __future__ import print_function
+
+
+def max_unsorted_subarray(a):
+    # find first disorder from the left
+    n = len(a)
+    for l in range(n-1):
+        if a[l+1] < a[l]:
+            break
+    else:
+        return -1
+
+    # find first disorder from the right
+    for r in range(n-1, 0, -1):
+        if a[r-1] > a[r]:
+            break
+
+    sub_min = min(a[l:r+1])
+    sub_max = max(a[l:r+1])
+
+    while l >= 0:
+        if a[l] <= sub_min:
+            break
+        l -= 1
+
+    while r < n:
+        if a[r] >= sub_max:
+            break
+        r += 1
+
+    return l, r, a[l:r]
+
+
+arr = [3,2,1]
+print(arr, max_unsorted_subarray(arr))
