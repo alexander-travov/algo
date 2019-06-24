a150fefabbcfb297cb55ae11a210cad63c944d46
Alexander Travov <alexander.travov@gmail.com>
2019-06-24 15:30:27 +0300

    Generate all Parentheses II problem.

---
+++ InterviewBit/Backtracking/GenerateAllParenthesesII.py
@@ -0,0 +1,29 @@
+"""
+Generate all Parentheses II
+===========================
+
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses of length 2*n.
+
+For example, given n = 3, a solution set is:
+
+"((()))", "(()())", "(())()", "()(())", "()()()"
+Make sure the returned list of strings are sorted.
+"""
+
+from __future__ import print_function
+import sys
+
+
+def generate_parentheses(n):
+    if n == 0:
+        yield ''
+    else:
+        for k in range(n, 0, -1):
+            for par1 in generate_parentheses(k-1):
+                for par2 in generate_parentheses(n-k):
+                    yield '(' + par1 + ')' + par2
+
+
+n = int(sys.argv[1]) if len(sys.argv)==2 else 4
+for par in generate_parentheses(n):
+    print(par)
