68e8ccce2e24bf4b4853cc7d395b0971d6174c64
Alexander Travov <alexander.travov@gmail.com>
2019-06-24 16:46:50 +0300

    Comment Sudoku problem solution.

--- InterviewBit/Backtracking/Sudoku.py
+++ InterviewBit/Backtracking/Sudoku.py
@@ -13,20 +13,24 @@ from __future__ import print_function
 class Sudoku:
     def __init__(self, puzzle):
         self.cells = [
-            [0 if c=='x' else int(c) for c in line]
+            [0 if c=='.' else int(c) for c in line]
             for line in puzzle
         ]
 
-        self.num_set_cells = sum(self.cells[r][c] != 0 for r in range(9) for c in range(9))
+        self.num_set_cells = sum(
+            self.cells[r][c] != 0
+            for r in range(9)
+            for c in range(9)
+        )
 
         self.choices = [
-            [self._init_choices(r, c) for c in range(9)]
+            [self.reset_choices(r, c) for c in range(9)]
             for r in range(9)
         ]
 
-    def _init_choices(self, row, col):
+    def reset_choices(self, row, col):
         ch = [True] * 10
-        ch[0] = False
+        ch[0] = False # Ficticious zero cell
 
         for c in range(9):
             n = self.cells[row][c]
@@ -46,7 +50,7 @@ class Sudoku:
 
     def __str__(self):
         return '\n'.join(
-            ''.join(str(v) if v else 'x' for v in self.cells[r])
+            ''.join(str(v) if v else '.' for v in self.cells[r])
             for r in range(9)
         )
 
@@ -69,30 +73,30 @@ class Sudoku:
                 self.choices[r+i][c+j][val] = False
 
     def free_cell(self, row, col):
-        val = self.cells[row][col]
         self.cells[row][col] = 0
         self.num_set_cells -= 1
 
         for c in range(9):
-            self.choices[row][c] = self._init_choices(row, c)
+            self.choices[row][c] = self.reset_choices(row, c)
 
         for r in range(9):
-            self.choices[r][col] = self._init_choices(r, col)
+            self.choices[r][col] = self.reset_choices(r, col)
 
         r, c = 3*(row//3), 3*(col//3)
         for i in range(3):
             for j in range(3):
-                self.choices[r+i][c+j] = self._init_choices(r+i, c+j)
+                self.choices[r+i][c+j] = self.reset_choices(r+i, c+j)
 
     def num_choices(self, row, col):
         return sum(self.choices[row][col])
 
 
 def solve(sudoku):
-    # print(sudoku.num_set_cells)
+    # All cells are set. Solution is found.
     if sudoku.num_set_cells == 81:
         return sudoku
 
+    # Find empty cell with minimum number of available choices.
     min_num_choices = 10
     min_r, min_c = -1, -1
 
@@ -106,32 +110,31 @@ def solve(sudoku):
                 min_r = r
                 min_c = c
 
+    # Not a solution. One of the previous guesses was wrong.
     if min_num_choices == 0:
         return None
 
     for val in range(1, 10):
+        # Try to solve sudoku for every available choice
         if not sudoku.choices[min_r][min_c][val]:
             continue
-        # print(sudoku)
-        # print('In pos: ({}, {}) setting {} from {} choices.'.format(
-        #     min_r, min_c, val, sudoku.choices[min_r][min_c]))
         sudoku.set_cell(min_r, min_c, val)
         solution = solve(sudoku)
         if solution:
             return solution
-        # print('Deadend: Free cell: ({}, {})'.format(min_r, min_c))
+        # If solution is not found - reset cell and try next choice.
         sudoku.free_cell(min_r, min_c)
 
 
 s = Sudoku([
-    '6xx3xxxxx',
-    'x13x6xxxx',
-    'xxxx9xx8x',
-    '4xxxx5xxx',
-    'x6xxx4x9x',
-    'x3xxxx7xx',
-    '8xxxxx25x',
-    'xxx8x1xx9',
-    'x4x9xxxx1',
+    '6..3.....',
+    '.13.6....',
+    '....9..8.',
+    '4....5...',
+    '.6...4.9.',
+    '.3....7..',
+    '8.....25.',
+    '...8.1..9',
+    '.4.9....1',
 ])
 print(solve(s))
