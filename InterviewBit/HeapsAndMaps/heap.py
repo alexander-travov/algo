6324c03d88bbe00e2ca09e1a860e8e2b3f8b8ae7
Alexander Travov <alexander.travov@gmail.com>
2019-06-29 17:52:25 +0300

    Extract heap module.

---
+++ InterviewBit/HeapsAndMaps/heap.py
@@ -0,0 +1,44 @@
+class MinHeap(list):
+    def _swap(self, i, j):
+        tmp = self[i]
+        self[i] = self[j]
+        self[j] = tmp
+
+    def _sift_up(self, ind):
+        if ind == 0:
+            return
+        parent = (ind-1) // 2
+        if self[ind][0] < self[parent][0]:
+            self._swap(ind, parent)
+            self._sift_up(parent)
+
+    def _sift_down(self, ind):
+        left_ch = 2*ind + 1
+        right_ch = 2*ind + 2
+        if left_ch >= len(self):
+            return
+        min_ch = left_ch
+        if right_ch < len(self) and self[right_ch][0] < self[left_ch][0]:
+            min_ch = right_ch
+        if self[ind] > self[min_ch]:
+            self._swap(ind, min_ch)
+            self._sift_down(min_ch)
+
+    def insert(self, key, value):
+        self.append((key, value))
+        self._sift_up(len(self)-1)
+
+    def get_min(self):
+        if not self:
+            return
+        return self[0]
+
+    def extract_min(self):
+        if not self:
+            return
+        min_kv = self[0]
+        last_kv = self.pop()
+        if self:
+            self[0] = last_kv
+            self._sift_down(0)
+        return min_kv
