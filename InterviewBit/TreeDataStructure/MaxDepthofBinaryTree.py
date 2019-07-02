71414dc64f928308bd10b0ca32283b9c159dfe13
Alexander Travov <alexander.travov@gmail.com>
2019-07-02 10:49:30 +0300

    Fix problem description.

--- InterviewBit/TreeDataStructure/MaxDepthofBinaryTree.py
+++ InterviewBit/TreeDataStructure/MaxDepthofBinaryTree.py
@@ -1,18 +1,18 @@
 """
-Min Depth of Binary Tree
+Max Depth of Binary Tree
 ========================
 
-Given a binary tree, find its minimum depth.
+Given a binary tree, find its maximum depth.
 
-The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+The maximum depth of a binary tree is the number of nodes along the longest path from the root node down to the farthest leaf node.
 
- NOTE : The path has to end on a leaf node. 
+ NOTE : The path has to end on a leaf node.
 Example :
 
          1
         /
        2
-min depth = 2.
+max depth = 2.
 """
 
 from __future__ import print_function
