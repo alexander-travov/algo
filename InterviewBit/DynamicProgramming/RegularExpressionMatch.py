d2b22994a65b796009b48240c0397d51b8982432
Alexander Travov <alexander.travov@gmail.com>
2019-09-14 13:15:47 +0300

    Regular Expression Match Problem.

---
+++ InterviewBit/DynamicProgramming/RegularExpressionMatch.py
@@ -0,0 +1,96 @@
+# -*- coding: utf8 -*-
+"""
+Regular Expression Match
+========================
+
+Implement wildcard pattern matching with support for ‘?’ and ‘*’ for strings A and B.
+
+’?’ : Matches any single character.
+‘*’ : Matches any sequence of characters (including the empty sequence).
+The matching should cover the entire input string (not partial).
+
+Input Format:
+
+The first argument of input contains a string A.
+The second argument of input contains a string B.
+Output Format:
+
+Return 0 or 1:
+    => 0 : If the patterns do not match.
+    => 1 : If the patterns match.
+Constraints:
+
+1 <= length(A), length(B) <= 9e4
+Examples :
+
+Input 1:
+    A = "aa"
+    B = "a"
+
+Output 1:
+    0
+
+Input 2:
+    A = "aa"
+    B = "aa"
+
+Output 2:
+    1
+
+Input 3:
+    A = "aaa"
+    B = "aa"
+
+Output 3:
+    0
+
+Input 4:
+    A = "aa"
+    B = "*"
+
+Output 4:
+    1
+
+Input 5:
+    A = "aa"
+    B = "a*"
+
+Output 5:
+    1
+
+Input 6:
+    A = "ab"
+    B = "?*"
+
+Output 6:
+    1
+
+Input 7:
+    A = "aab"
+    B = "c*a*b"
+
+Output 7:
+    0
+"""
+
+
+def match(s, pat):
+    if not s and not pat:
+        return True
+    if not s and pat or s and not pat:
+        return False
+
+    if pat[0] not in ('?', '*'):
+        if s[0] != pat[0]:
+            return False
+        else:
+            return match(s[1:], pat[1:])
+
+    elif pat[0] == '?':
+        return match(s[1:], pat[1:])
+
+    else:
+        return any(match(s[n:], pat[1:]) for n in range(len(s)+1))
+
+
+print(match('abc', '?*c'))
